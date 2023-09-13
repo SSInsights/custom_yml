6dbe776021f3adfa15cfa4f6fc1cbf63e3519daf
SSInsights <shivpal.aiml50@gmail.com>
2023-09-13 18:20:53 +0530

    updated pytest added after removing warnings

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ This is testinng for custom YAML for CI
 
 """
 def custom():
+    """
+    test function added
+    """
     return "Hello"
--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
+
+"""
+unite test module
+
+"""
 from app import custom
 
 def test_fun():
+
+    """
+    unit test case1
+    """
     assert custom() == "Hello"
-
-
